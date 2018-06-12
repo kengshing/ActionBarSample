41e6ffa5d0ff01216e9e891439f4b4769b9e93a7
Aw <kengshing.aw@orcacreation.net>
2018-06-12 18:09:33 +0800

    180612 Desktop Implemented spinner. Spinner and Favorite will reset with new Activity (screen rotate). Probably need onSaveInstanceState() and onRestoreInstanceState().

--- app/src/main/java/net/orcacreation/actionbarsample/MainActivity.java
+++ app/src/main/java/net/orcacreation/actionbarsample/MainActivity.java
@@ -6,9 +6,10 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -16,6 +17,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String quickAccessValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +35,45 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+
+        //Setting up the spinner
         MenuItem item = menu.findItem(R.id.action_qa);
         Spinner qASpinner = (Spinner) item.getActionView();
         ArrayAdapter<CharSequence> qAAdaptor = ArrayAdapter.createFromResource(this, R.array.quick_acces_list, R.layout.spinner_text_layout);
         qASpinner.setAdapter(qAAdaptor);
-        //return true;
+
+        qASpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                switch (position) {
+                    case 0:
+                        quickAccessValue = getResources().getStringArray(R.array.quick_acces_list)[0];
+                        refreshScreen();
+                        break;
+
+                    case 1:
+                        quickAccessValue = getResources().getStringArray(R.array.quick_acces_list)[1];
+                        refreshScreen();
+                        break;
+                    case 2:
+                        quickAccessValue = getResources().getStringArray(R.array.quick_acces_list)[2];
+                        refreshScreen();
+                        break;
+                    case 3:
+                        quickAccessValue = getResources().getStringArray(R.array.quick_acces_list)[3];
+                        refreshScreen();
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                quickAccessValue = getString(R.string.quick_access_not_selected);
+                refreshScreen();
+            }
+        });
+        //Setting up the spinner - end
 
         return super.onCreateOptionsMenu(menu);
     }
@@ -80,6 +117,12 @@ public class MainActivity extends AppCompatActivity {
                 getString(R.string.settings_list_key),
                 getString(R.string.settings_list_default));
         txtListView.setText(settingList);
+
+        TextView txtQuickAccessView = findViewById(R.id.txt_quick_access);
+        if (quickAccessValue != null) {
+            txtQuickAccessView.setText(quickAccessValue);
+        }
+
     }
 
     private void refreshFavoriteImage (int resourceId){
